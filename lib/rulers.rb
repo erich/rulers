e9e7083560b2a2124180c819813f246da924fd1c
Erich Kaderka <erich.kaderka@gmail.com>
2012-07-14 15:57:46 +0100

    Added rack call

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,5 +1,9 @@
 require "rulers/version"
 
 module Rulers
-  # Your code goes here...
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'}, ["Hello from rulers"]]
+    end
+  end
 end
